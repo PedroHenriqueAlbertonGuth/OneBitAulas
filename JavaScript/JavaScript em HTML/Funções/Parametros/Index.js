8db4bbd0e5f7ea1b5fb97aa5eb69f304b367dddc
Pedro Henrique Alberton Guth <pedrohaguth943@gmail.com>
2023-10-25 12:33:51 -0300

    estudosssssssssssss

--- JavaScript/JavaScript em HTML/Funções/Parametros/Index.js
+++ JavaScript/JavaScript em HTML/Funções/Parametros/Index.js
@@ -33,7 +33,7 @@ function criarUsuario(nome, email, senha, tipoDeUsuario = "admin") {
 
 function novoUsuario(nome, tipo = "admin", email, senha) {
   const usuario = {
-    nome, //mesmo que nome: nome
+    nome,
     email,
     senha,
     tipo,
@@ -42,3 +42,31 @@ function novoUsuario(nome, tipo = "admin", email, senha) {
   console.log(usuario)
 }
 
+criarUsuario("Pedro", "pedrohaguth943@gmail.com", "1234")
+novoUsuario("Pedro", "pedrohaguth943@gmail.com", "1234")
+
+function manyParameters(nome, telefone, endereco, aniversario, email, senha) {
+
+}
+
+manyParameters("nome", "telefone", "endereco", "aniversario", "email", "senha")
+
+function objectWithParameter(user) {
+  user.nome
+  user.telefone
+  user.email
+  user.senha
+  user.endereco
+  user.aniversario
+}
+
+const userData = {
+  nome: "",
+  telefone: "",
+  email: "",
+  senha: "",
+  endereco: "",
+  aniversario: "",
+}
+
+objectWithParameter(userData)
